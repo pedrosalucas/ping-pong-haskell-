b73b04d1107ed63e43e9b0a97c64cace880f1be2
pedrosalucas <54561127+pedrosalucas@users.noreply.github.com>
2019-08-28 00:31:22 -0300

    Add files via upload

---
+++ Main.hs
@@ -0,0 +1,102 @@
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game
+
+largura, altura, offset :: Int
+largura = 600
+altura = 300
+offset = 100
+
+larguraF, alturaF, offsetF :: Float
+larguraF = 600
+alturaF = 300
+offsetF = 100
+
+larguraBor = (larguraF - 30)
+alturaBor = 10
+
+diametroBola = 10
+
+larguraRaq = 26
+alturaRaq = 86
+
+fps = 60
+
+window :: Display
+window = InWindow "Ping Pong" (largura, altura) (offset, offset)
+
+background :: Color
+background = black
+
+bolaImagem :: Picture
+bolaImagem = color cor $ circleSolid diametroBola
+ where cor = (dark white)
+
+borda :: Float -> Picture
+borda offset = translate 0 offset $ color corBorda $ rectangleSolid (larguraBor) (alturaBor)
+ where corBorda = greyN 0.5
+
+data Movimento = Move { localBola :: (Float, Float), velBola :: (Float, Float),
+                        raqueteDir :: Float, raqueteEsq :: Float}
+ deriving Show
+
+estadoInicial :: Movimento
+estadoInicial = Move { localBola = (-larguraF/2 + diametroBola/2 + larguraRaq, (-30)),
+                       velBola = (55, 45) , raqueteDir = 0 , raqueteEsq = 0 }
+
+moveBola :: Float -> Movimento -> Movimento
+moveBola t move = move { localBola = (x1, y1) }
+ where (x, y) = localBola move
+       (vx, vy) = velBola move
+       x1 = x + vx * t
+       y1 = y + vy * t
+
+colideBorda :: Movimento-> Movimento
+colideBorda move = move { velBola = (vx,vy1) }
+  where (vx, vy) = velBola move
+        (x, y) = localBola move
+        vy1 = if colideCima || colideBaixo then (-vy) else vy
+        colideBaixo = y - diametroBola <= -alturaF/2 + alturaBor
+        colideCima = y + diametroBola >= alturaF/2 - alturaBor
+
+colideRaq :: Movimento -> Movimento
+colideRaq move = move { velBola = (vx1, vy) }
+ where (vx, vy) = velBola move
+       (x, y) = localBola move
+       posEsq = raqueteEsq move
+       posDir = raqueteDir move
+       vx1 = if colideEsq || colideDir then eixoY else vx
+       colideEsq = x - diametroBola/2 <= -larguraF/2 + larguraRaq/2
+       colideDir = x + diametroBola/2 >= larguraF/2 - larguraRaq/2
+       eixoY = if colideEY posEsq y || colideEY posDir y  then (-vx) else vx
+
+colideEY :: Float -> Float -> Bool
+colideEY p a = if a > p - alturaRaq/2 && a < p + alturaRaq/2 then True else False
+
+main :: IO ()
+main = play window background fps estadoInicial desenho controle update
+
+update :: Float -> Movimento -> Movimento
+update t =  colideBorda . moveBola t . colideRaq
+
+raquete:: Color -> Float -> Float -> Picture
+raquete corExt x y = pictures [ translate (x) (y) $ color corExt $ rectangleSolid (larguraRaq) (alturaRaq),
+                               translate (x) (y) $ color corInterna $ rectangleSolid (larguraRaq - 6) (alturaRaq - 6) ]
+ where corInterna = light (light black)
+
+controle :: Event -> Movimento -> Movimento
+controle (EventKey (Char 'r') _ _ _) move = move { localBola = (0, 0), velBola = (0, 0) }
+controle (EventKey (Char 'f') _ _ _) move = move { localBola = (0, 0), velBola = (-50, 40) }
+controle (EventKey (Char 'o') Up _ _) move = if (raqueteDir move) + alturaRaq/2 <= alturaF/2 then move { raqueteDir = 15 + (raqueteDir move) } else move { raqueteDir = (raqueteDir move) }
+controle (EventKey (Char 'w') Up _ _) move = move { raqueteEsq = 15 +(raqueteEsq move) }
+controle (EventKey (Char 's') Down _ _) move = move { raqueteEsq = -15 +(raqueteEsq move) }
+controle (EventKey (Char 'l') Down _ _) move = move { raqueteDir = -15 +(raqueteDir move) }
+controle _ move = move
+
+
+desenho :: Movimento -> Picture
+desenho move = pictures [ bola, bordas, raqEsq, raqDir ]
+ where bordas = pictures [borda (alturaF/2), borda (-alturaF/2)]
+       bola = uncurry translate (localBola move) $ bolaImagem
+       raqEsq = raquete green (-larguraF/2) $ raqueteEsq move
+       raqDir = raquete red (larguraF/2) $ raqueteDir move
